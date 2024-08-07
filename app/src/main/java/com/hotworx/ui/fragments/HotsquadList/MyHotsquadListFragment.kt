d650d9888d48b32b8ec284a36c220551a8dfd631
zerafshanhakmani689 <zerafshan.hakmani123@gmail.com>
2024-08-07 04:18:28 -0700

    generic adapter added

--- app/src/main/java/com/hotworx/ui/fragments/HotsquadList/MyHotsquadListFragment.kt
+++ app/src/main/java/com/hotworx/ui/fragments/HotsquadList/MyHotsquadListFragment.kt
@@ -7,29 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import com.hotworx.R
 import com.hotworx.databinding.FragmentHotsquadCreateBinding
+import com.hotworx.databinding.FragmentMyHotsquadListBinding
 import com.hotworx.ui.fragments.BaseFragment
 import com.hotworx.ui.views.TitleBar
 
 
 class MyHotsquadListFragment : BaseFragment() {
-    private var _binding: FragmentHotsquadCreateBinding? = null
+    private var _binding: FragmentMyHotsquadListBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentHotsquadCreateBinding.inflate(inflater, container, false)
+        _binding = FragmentMyHotsquadListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnCreateHotsquad.setOnClickListener {
-            val addListFragment = AddListFragment()
-            dockActivity.replaceDockableFragment(addListFragment)
-        }
     }
 
     override fun setTitleBar(titleBar: TitleBar) {
